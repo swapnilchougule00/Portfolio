e7e48156fb2d16518a1c41e7500b86e293705bf9
swapnil <swapnilchougule00@gmail.com>
2023-01-02 16:11:09 +0530

    changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,13 +6,13 @@ import Project from "./Project";
 function Projects() {
   return (
     <motion.div  id='projects'
-      className="h-screen relative flex flex-col text-center 
+      className="no-scrollbar h-screen relative flex flex-col text-center 
     overflow-hidden max-w-full justify-evenly px-10 mx-auto items-center "
     >
       <p className="absolute text-2xl top-24 text-gray-500 uppercase tracking-[20px]">
         Projects
       </p>
-        <div className="w-full flex space-x-5 overflow-x-scroll  ">
+        <div className=" w-full flex space-x-5 overflow-x-scroll scrollbar-hide  ">
       <Project/>
         </div>
       
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [    require('tailwind-scrollbar'),
-],
+  plugins: [ require('tailwind-scrollbar-hide')],
 }
 
